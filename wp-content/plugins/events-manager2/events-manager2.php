8986ab588bf4461b81e28aeece443a19b6d3195e
Elizabeth Buckwalter <elizabeth.buckwalter@gmail.com>
2010-03-30 10:43:51 -0700

    Since interval is a reserved word, all references had to be changed to intervals in Event Manager

--- wp-content/plugins/events-manager2/events-manager2.php
+++ wp-content/plugins/events-manager2/events-manager2.php
@@ -578,7 +578,7 @@ function dbem_create_recurrence_table() {
 	$table_name = $wpdb->prefix.RECURRENCE_TBNAME;
 
 	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-		// oops.  interval is a reserved word in sql.
+		// oops.  intervals is a reserved word in sql.
 		$sql = "CREATE TABLE ".$table_name." (
 			recurrence_id mediumint(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 			event_id mediumint(9) NOT NULL,
@@ -586,7 +586,7 @@ function dbem_create_recurrence_table() {
 			freq tinytext NOT NULL,
 			byday tinyint NOT NULL,
 			byweekno tinyint NOT NULL,
-			intervals tinyint NOT NULL
+			intervalss tinyint NOT NULL
 			);";
 		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 		dbDelta($sql);
