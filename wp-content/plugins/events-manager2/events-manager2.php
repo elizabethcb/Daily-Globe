c66bc03f636723eaccaaf8e4086b4b7b59462249
Elizabeth Buckwalter <elizabeth@slatetechpdx.com>
2010-04-05 12:19:44 -0700

    testing branching

--- wp-content/plugins/events-manager2/events-manager2.php
+++ wp-content/plugins/events-manager2/events-manager2.php
@@ -28,6 +28,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 /*************************************************/ 
 
+// I am testing the branching thing
 // Setting constants
 // In Events Class
 define('EVENTS_TBNAME','dbem2_events'); //TABLE NAME
