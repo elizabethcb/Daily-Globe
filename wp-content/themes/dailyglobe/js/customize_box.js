ab2d75d49b675f35f121b88dba39bb31c68e0c9e
Elizabeth Buckwalter <elizabeth.buckwalter@gmail.com>
2010-04-10 11:46:22 -0700

    adding new files

---
+++ wp-content/themes/dailyglobe/js/customize_box.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("a#customize_box").fancybox({
+		'speedIn'               : 600, 
+		'speedOut'              : 200,
+		'autoDimensions'        : true,
+		'overlayShow'           : true,
+		'type'                  : 'inline',
+		'hideOnContentClick'    : false
+
+    });
+});
